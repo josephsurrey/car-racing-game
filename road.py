21b2a80978791b52bf7678bb94b3d778710370e0
josephsurrey <josephsurrey2018@gmail.com>
2025-05-28 16:51:06 +1200

    Game - _update_game_state

--- road.py
+++ road.py
@@ -2,7 +2,7 @@ class Road:
     def __init__(self, road_image, screen_width, screen_height):
         pass
 
-    def update(self):
+    def update(self, current_road_speed):
         pass
 
     def draw(self):
